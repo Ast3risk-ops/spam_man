e5c0fb0e4fcc33d793cbe8aa8092a7927a191392
0xygen24 <83875983+0xygen24@users.noreply.github.com>
2022-05-07 15:12:46 -0400

    Added main.py script
    Open for business!

---
+++ main.py
@@ -0,0 +1,37 @@
+import keyboard
+from guizero import App, PushButton, Text
+
+
+def e() -> None:
+    app.destroy()
+    quit()
+
+
+
+def play() -> None:
+    recorded = keyboard.record(until='esc')
+    while True:
+        keyboard.play(recorded, speed_factor=10)
+app = App(title="Spam Man")
+Text(app, text="Record keystrokes until Esc. is pressed, then play them back")
+bong = PushButton(app, command=play, text="Record and play!")
+PushButton(app, command=e, text="Stop")
+app.display()
+
+
+
+
+
+
+# Free space for text cursor
+
+
+
+
+
+
+
+
+
+
+
