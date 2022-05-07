52aaf69494b5532c072f0521fe41252b1ac7e527
Oxygen24 <83875983+0xygen24@users.noreply.github.com>
2022-05-07 19:30:11 -0400

    Add bonus blocking bit

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from guizero import App, PushButton, Text
 
 
 def e() -> None:
-    app.destroy()
+    App.destroy()
     quit()
 
+def bloc() -> None:
+    keyboard.wait()
 
 
 def play() -> None:
@@ -16,6 +18,7 @@ app = App(title="Spam Man")
 Text(app, text="Record keystrokes until Esc. is pressed, then play them back")
 bong = PushButton(app, command=play, text="Record and play!")
 PushButton(app, command=e, text="Stop")
+PushButton(app, command=bloc, text="Block keyboard input")
 app.display()
 
 
