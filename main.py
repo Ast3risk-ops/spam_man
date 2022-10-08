58c8aaa746f8b6db3181a6c1d8d2e6a2378cfd65
Ast3risk-ops <spacedude609@pm.me>
2022-10-08 09:22:56 -0400

    Ask user how fast?

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import keyboard
-from guizero import App, PushButton, Text
+from guizero import App, PushButton, Text, Window, TextBox
 
 
 def e() -> None:
@@ -12,13 +12,16 @@ def bloc() -> None:
 
 def play() -> None:
     recorded = keyboard.record(until='esc')
+    sped_1 = app.question("How fast?", "Speed must be a number!", initial_value="10")
+    sped = int(sped_1)
     while True:
-        keyboard.play(recorded, speed_factor=10)
+        keyboard.play(recorded, speed_factor=sped)
 app = App(title="Spam Man")
 Text(app, text="Record keystrokes until Esc. is pressed, then play them back")
 bong = PushButton(app, command=play, text="Record and play!")
 PushButton(app, command=e, text="Stop")
 PushButton(app, command=bloc, text="Block keyboard input")
+
 app.display()
 
 
